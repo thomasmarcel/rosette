2d71290f57cba02bbbb20c2bb88bdbfc694a7cc7
Thomas Alcala Schneider <thomas.alcala@gmail.com>
2021-03-12 18:32:35 +0100

    feat: basic variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+
+    let base_endpoint = "https://translation.googleapis.com/language/translate/v2";
+    let args: Vec<String> = env::args().collect();
+    let api_key = match env::var("GOOGLE_API_KEY") {
+        Ok(val) => val,
+        Err(_e) => panic!("Set up the GOOGLE_API_KEY environment variable first"),
+    };
+
+    println!(
+        "endpoint: {:?}, args: {:?}, key: {:?}",
+        base_endpoint, args, api_key
+    );
 }
