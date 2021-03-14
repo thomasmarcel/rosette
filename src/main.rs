5214a4c102ac933e31bfd75eb2e68dd5aee888c9
Thomas Alcala Schneider <thomas.alcala@gmail.com>
2021-03-14 14:58:24 +0100

    feat: call the translate endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use std::env;
+use std::collections::HashMap;
 
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+use serde::Deserialize;
 
+#[derive(Deserialize, Debug)]
+struct Translation {
+    #[serde(alias = "translatedText")]
+    translated_text: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Translations {
+    translations: Vec<Translation>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Data {
+    data: Translations,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     let base_endpoint = "https://translation.googleapis.com/language/translate/v2";
     let args: Vec<String> = env::args().collect();
     let api_key = match env::var("GOOGLE_API_KEY") {
@@ -14,4 +33,29 @@ fn main() {
         "endpoint: {:?}, args: {:?}, key: {:?}",
         base_endpoint, args, api_key
     );
+
+    let query = "rust";
+    let source = "en";
+    let target = "fr";
+
+    let mut map = HashMap::new();
+    map.insert("q", query);
+    map.insert("source", source);
+    map.insert("target", target);
+    map.insert("key", &api_key);
+
+    let request_url = format!("{base}?key={key}&q={query}&source={source}&target={target}", base = base_endpoint, key = api_key, query = query, source = source, target = target);
+    // let request_url = format!("{base}?key={key}", base = base_endpoint, key = &api_key);
+
+    let client = reqwest::Client::new();
+
+    let response = client.post(&request_url).form(&map).send().await?;
+
+    let text_response = response.text().await?;
+
+    let translations= serde_json::from_str::<Data>(&text_response).unwrap();
+
+    println!("{:?}", translations);
+
+    Ok(())
 }
