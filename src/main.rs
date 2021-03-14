75d55438e14b28e0ea5edc610779caf14678ff7e
Thomas Alcala Schneider <thomas.alcala@gmail.com>
2021-03-14 16:23:49 +0100

    feat: translate a file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::collections::HashMap;
 use reqwest::Error;
 use serde::Deserialize;
 
+mod utils;
+use crate::utils::read_lines;
+
 #[derive(Deserialize, Debug)]
 struct Translation {
     #[serde(alias = "translatedText")]
@@ -20,24 +23,51 @@ struct Data {
     data: Translations,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let base_endpoint = "https://translation.googleapis.com/language/translate/v2";
+fn main() {
     let args: Vec<String> = env::args().collect();
     let api_key = match env::var("GOOGLE_API_KEY") {
         Ok(val) => val,
         Err(_e) => panic!("Set up the GOOGLE_API_KEY environment variable first"),
     };
 
+    if args.len() != 2 {
+        panic!("You need to pass a text file name");
+    }
+
     println!(
-        "endpoint: {:?}, args: {:?}, key: {:?}",
-        base_endpoint, args, api_key
+        "args: {:?}, key: {:?}",
+        args, api_key
     );
 
     let query = "rust";
-    let source = "en";
-    let target = "fr";
+    let source = "fr";
+    let target = "en";
 
+    // let translations = translate(query, source, target, &api_key);
+
+    // println!("{:?}", translations);
+
+    if let Ok(lines) = read_lines(&args[1]) {
+        for line in lines {
+            if let Ok(row) = line {
+                let parsed_row = row.replace("\u{a0}", "");
+                println!("{:?}", parsed_row);
+                let translation = match translate(&parsed_row, source, target, &api_key) {
+                    Ok(t) => {
+                        let t2 = String::from(&t.data.translations[0].translated_text);
+                        t2
+                    }
+                    Err(_) => String::from(""),
+                };
+                println!("{:?}", translation);
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn translate(query: &str, source: &str, target: &str, api_key: &str) -> Result<Data, Error> {
+    let base_endpoint = "https://translation.googleapis.com/language/translate/v2";
     let mut map = HashMap::new();
     map.insert("q", query);
     map.insert("source", source);
@@ -55,7 +85,5 @@ async fn main() -> Result<(), Error> {
 
     let translations= serde_json::from_str::<Data>(&text_response).unwrap();
 
-    println!("{:?}", translations);
-
-    Ok(())
+    Ok(translations)
 }
