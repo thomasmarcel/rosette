63e48091f8e2ed9e3d672e7319aaa5d5ac6dc6b4
Thomas Alcala Schneider <thomas.alcala@gmail.com>
2021-03-14 20:22:05 +0100

    feat: rename

---
+++ README.md
@@ -0,0 +1,15 @@
+# Rosetta
+
+## Installation
+
+### With Cargo
+
+1. Clone this repository
+1. Run `cargo build -- release`
+
+
+## TODO
+
+* [ ] Make sure that the languages are valid
+* [X] Don't send empty lines to the API
+* [ ] Cross platform CI/CD
--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,12 @@ struct Data {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
     let api_key = match env::var("GOOGLE_API_KEY") {
         Ok(val) => val,
         Err(_e) => panic!("Set up the GOOGLE_API_KEY environment variable first"),
     };
+
+    let args: Vec<String> = env::args().collect();
 
     if args.len() != 4 {
         panic!("You need to pass a text file name, a source and a target language");
@@ -50,7 +51,7 @@ fn main() {
         for line in lines {
             if let Ok(row) = line {
                 let parsed_row = row.replace("\u{a0}", "");
-                println!("{:?}", parsed_row);
+                // println!("{}", parsed_row);
                 let translation = match translate(&parsed_row, source, target, &api_key) {
                     Ok(t) => {
                         let t2 = String::from(&t.data.translations[0].translated_text);
@@ -58,7 +59,7 @@ fn main() {
                     }
                     Err(_) => String::from(""),
                 };
-                println!("{:?}", translation);
+                println!("{}", translation);
             }
         }
     }
@@ -66,23 +67,37 @@ fn main() {
 
 #[tokio::main]
 async fn translate(query: &str, source: &str, target: &str, api_key: &str) -> Result<Data, Error> {
-    let base_endpoint = "https://translation.googleapis.com/language/translate/v2";
-    let mut map = HashMap::new();
-    map.insert("q", query);
-    map.insert("source", source);
-    map.insert("target", target);
-    map.insert("key", &api_key);
+    let translations = match query.len() > 0 {
+        true => {
+            let base_endpoint = "https://translation.googleapis.com/language/translate/v2";
+            let mut map = HashMap::new();
+            map.insert("q", query);
+            map.insert("source", source);
+            map.insert("target", target);
+            map.insert("key", &api_key);
 
-    let request_url = format!("{base}?key={key}&q={query}&source={source}&target={target}", base = base_endpoint, key = api_key, query = query, source = source, target = target);
-    // let request_url = format!("{base}?key={key}", base = base_endpoint, key = &api_key);
+            let request_url = format!("{base}?key={key}&q={query}&source={source}&target={target}", base = base_endpoint, key = api_key, query = query, source = source, target = target);
 
-    let client = reqwest::Client::new();
+            let client = reqwest::Client::new();
 
-    let response = client.post(&request_url).form(&map).send().await?;
+            let response = client.post(&request_url).form(&map).send().await?;
 
-    let text_response = response.text().await?;
+            let text_response = response.text().await?;
 
-    let translations= serde_json::from_str::<Data>(&text_response).unwrap();
+            serde_json::from_str::<Data>(&text_response).unwrap()
+        },
+        false => {
+            let tr =  Translation { translated_text: String::from("") };
+            let trs: Vec<Translation> = vec!(tr);
+            let data = Translations {
+                translations: trs,
+            };
+            let empty_output = Data {
+                data: data,
+            };
+            empty_output
+        },
+    };
 
     Ok(translations)
 }
